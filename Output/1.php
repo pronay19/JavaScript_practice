30d66be0cdc2f58b9798630a28ff8d1036a06b07
Pronay Saha <60335986+pronay19@users.noreply.github.com>
2020-03-25 12:21:01 +0600

    Add files via upload

---
+++ Output/1.php
@@ -0,0 +1,17 @@
+ <!DOCTYPE html>
+<html>
+<body>
+
+<h1>My First Web Page</h1>
+<p>My First Paragraph</p>
+
+<p id="demo"></p>
+
+<script>
+//document.getElementById("demo").innerText = 5 + 6;
+//document.write(5 + 6);
+document.getElementById("demo").innerHTML = 5 + 6;
+</script>
+
+</body>
+</html> 
